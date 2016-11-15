a9b9d0c42bbdff1a65abe58cd825dcd24c8bb6ba
Tibor Krizsak <tybe3e@gmail.com>
2016-11-15 16:37:29 +0100

    Added MatrixScalarProd, MatrixProd (not tested yet).

--- feladat.cpp
+++ feladat.cpp
@@ -26,14 +26,15 @@ class MatrixSub;
 template<class T, int N, int M, class Left, class Right>
 class MatrixProd;
 
+template<class T, int N, int M, class Mtx>
+class MatrixScalarProd;
+
 template<class T, int N, int M>
 class my_matrix : matrix_base {
 public:
     /** A sablonpéldány rendelkezik default konstruktorral. */
     T data[N][M];
-    my_matrix(){
-      cout << "N: " << N << ", M: " << M << endl;
-    }
+    my_matrix(){}
     /**
      * A mátrix elemeihez való hozzáférést a kétparaméteres függvényhívás
      * operátor felüldefiniálása biztosítja.
@@ -67,6 +68,16 @@ public:
 
     template<class Left, class Right>
     my_matrix<T,N,M>& operator=(const MatrixProd<T,N,M,Left,Right>& v) {
+      for (int i = 0; i < N; ++i){
+        for (int j = 0; j < M; ++j){
+          data[i][j] = v(i,j);
+        }
+      }
+      return *this;
+    }
+
+    template<class Mtx>
+    my_matrix<T,N,M>& operator=(const MatrixScalarProd<T,N,M,Mtx>& v) {
       for (int i = 0; i < N; ++i){
         for (int j = 0; j < M; ++j){
           data[i][j] = v(i,j);
@@ -165,42 +176,92 @@ class MatrixProd{
 public:
   MatrixProd(const Left &l, const Right &r) : left(l), right(r) {}
   // operator[], amely visszaadja a bal es jobb operandus i-dik pozícióján levõ elem összeget
-  T operator()(int i, int j) const { return left(i,j) * right(i,j); }
+  T operator()(int x, int y) const {
+    int sum = 0;
+
+    for (int i=0; i<M; i++){
+      sum += left(x,i) * right(i,y);
+    }
+
+    return sum;
+  }
 };
 
 
-// case: mtx1 + mtx2
-template<class T, int N, int M>
-inline auto operator*(const my_matrix<T,N,M> &l, const my_matrix<T,N,M> &r) {
-  return MatrixProd<T, N, M, my_matrix<T,N,M>, my_matrix<T,N,M> >(l,r);
+// case: mtx1 * mtx2
+template<class T, int N, int M, int K>
+inline auto operator*(const my_matrix<T,N,M> &l, const my_matrix<T,M,K> &r) {
+  return MatrixProd<T, N, K, my_matrix<T,N,M>, my_matrix<T,M,K> >(l,r);
 }
 
-// case: mtx1 + mtx2 + mtx3 (MatrixProd exists)
-template<class T, int N, int M, class Left, class Right>
+/*
+// case: mtx1 * mtx2 * mtx3 (MatrixProd exists)
+template<class T, int K, int M, class Left, class Right>
 inline auto operator*(const MatrixProd<T,N,M,Left,Right> &l, const my_matrix<T,N,M> &r) {
   return MatrixProd<T, N, M, MatrixProd<T,N,M,Left,Right>, my_matrix<T,N,M> >(l,r);
 }
 
-// case: mtx1 + (mtx2 + mtx3)
-template<class T, int N, int M, class Left, class Right>
+// case: mtx1 * (mtx2 * mtx3)
+template<class T, int K, int M, class Left, class Right>
 inline auto operator*(const my_matrix<T,N,M> &l, const MatrixProd<T,N,M,Left,Right> &r) {
   return MatrixProd<T, N, M, my_matrix<T,N,M>, MatrixProd<T,N,M,Left,Right> >(l,r);
 }
-
+*/
 
 /**
  * Példa a mátrix sablon egy felhasználására. A beadott feladatban nem szabad
  * szerepelnie.
  */
 
+
+
+
+
+
+
+
+
+ template<class T, int N, int M, class Mtx>
+ class MatrixScalarProd{
+   const Mtx &mtx;
+   const T &scalar;
+ public:
+   MatrixScalarProd(const Mtx &m, const T &s) : mtx(m), scalar(s) {}
+
+   T operator()(int i, int j) const { return scalar * mtx(i,j); }
+ };
+
+ template<class T, int N, int M>
+ inline auto operator*(const my_matrix<T,N,M> &m, const T &s) {
+   return MatrixScalarProd<T, N, M, my_matrix<T,N,M> >(m,s);
+ }
+
+ template<class T, int N, int M>
+ inline auto operator*(const T &s, const my_matrix<T,N,M> &m) {
+   return MatrixScalarProd<T, N, M, my_matrix<T,N,M> >(m,s);
+ }
+
+
+
+
+
+
 int main() {
     my_matrix<int,2,2> mtx1;
     my_matrix<int,2,2> mtx2;
 
     my_matrix<int,2,2> mtx3;
+    my_matrix<int,2,3> mtx4;
     mtx1(1,1) = 55;
     mtx2(1,1) = 11;
+
     mtx3 = mtx1 - mtx2 - mtx1 - mtx2;
+    cout << "mtx3: " << mtx3(1,1) << endl;
+
+    mtx3 = 5 * mtx3;
+    cout << "mtx3: " << mtx3(1,1) << endl;
+
+    mtx3 = mtx1 * mtx2;
     cout << "mtx3: " << mtx3(1,1) << endl;
 
 
